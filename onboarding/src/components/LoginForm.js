18225294e5c1c78614627146ef63d8a26ffa0bc1
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-07-17 16:10:08 -0600

    validation is functional

--- onboarding/src/components/LoginForm.js
+++ onboarding/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
-function LoginForm() {
+function LoginForm({errors, touched}) {
     return(
         <Form>
             <Field 
@@ -11,13 +12,20 @@ function LoginForm() {
                 placeholder='Enter Name Here'
                 autoComplete='off'
             />
-            <Field type='email' name='email' placeholder='Enter Email Here' />
+            <p>{touched.name && errors.name}</p>
+            <Field 
+                type='email' 
+                name='email' 
+                placeholder='Enter Email Here' 
+            />
+            <p>{touched.email && errors.email}</p>
             <Field 
                 type='password' 
                 name='password' 
                 placeholder='Enter Password Here' 
                 autoComplete='off'
             />
+            <p>{touched.password && errors.password}</p>
             {/* <Field type='checkbox' name='terms' /> */}
             <button type='submit' >Submit!</button>
         </Form>
@@ -33,8 +41,21 @@ const FormikLoginForm = withFormik({
             email: email || '',
         };
     },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .min(3, 'Name must be at least 3 characters')
+            .required('Name is required'),
+        email: Yup.string()
+            .required('Email is required'),
+        password: Yup.string()
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+    }),
+
     handleSubmit: values => {
         console.log(values);
+        
     }
 
 })(LoginForm);
